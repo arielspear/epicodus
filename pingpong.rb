01fd03ee88ea669274d6b81ce22320c068a103a1
Ariel Spear <simpleredfox@gmail.com>
2015-07-22 00:10:47 -0700

    PingPong work on command line.

--- pingpong.rb
+++ pingpong.rb
@@ -4,7 +4,17 @@ num = gets.to_i
 i = 0
 
 puts "Thanks, let's play ping-pong."
+
 while i < num do
   i += 1
+  if (i % 3 == 0) && (i % 5 == 0)
+    puts "Ping-Pong"
+  elsif (i % 3 == 0)
+    puts "Ping"
+  elsif (i % 5 == 0)
+    puts "Pong"
+  else
   puts (i)
+  end
+
 end
