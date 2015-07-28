ca499c9a78a979e664cde652780a8b01d48330c6
Ariel Spear <simpleredfox@gmail.com>
2015-07-28 02:14:38 -0700

    Sings to me

---
+++ Prework/Lyrics.java
@@ -0,0 +1,3 @@
+public class Lyrics {
+  String[] lyrics = new String[] { "She was trapped in a mirror", "And it couldn't be clearer", "She wanted to leave this place", "And get herself back in space", "And dad you might think she's a criminal", "But her friendship comes through subliminal", "Lapis Lazuli, you fled into the bottom of the sea", "Lapis Lazuli, you were so mad, but then you came around to me" };
+}
---
+++ Prework/Sing.java
@@ -0,0 +1,25 @@
+public class Sing {
+  String[] Lyrics = { "She was trapped in a mirror", "And it couldn't be clearer", "She wanted to leave this place", "And get herself back in space", "And dad you might think she's a criminal", "But her friendship comes through subliminal", "Lapis Lazuli, you fled into the bottom of the sea", "Lapis Lazuli, you were so mad, but then you came around to me" };
+
+  public static void main(String[] args) {
+    System.out.println("She was trapped in a mirror");
+    System.out.println("And it couldn't be clearer");
+    System.out.println("She wanted to leave this place");
+    System.out.println("And get herself back in space");
+    System.out.println("And dad you might think she's a criminal");
+    System.out.println("But her friendship comes through subliminal");
+    System.out.println("Lapis Lazuli, you fled into the bottom of the sea");
+    System.out.println("Lapis Lazuli, you were so mad, but then you came around to me");
+  }
+  // public static void main(String[] Lyrics) {
+    // System.out.println(Lyrics[0]);
+    // System.out.println(lyrics[1]);
+    // System.out.println(lyrics[2]);
+    // System.out.println(lyrics[3]);
+    // System.out.println(lyrics[4]);
+    // System.out.println(lyrics[5]);
+    // System.out.println(lyrics[6]);
+    // System.out.println(lyrics[7]);
+    // System.out.println(lyrics[8]);
+  // }
+}
