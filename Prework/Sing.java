977f6768d87a83f30d07f0b8c94ddc60216b74c1
Ariel Spear <simpleredfox@gmail.com>
2015-07-29 16:24:22 -0700

    Iterates through array.

--- Prework/Sing.java
+++ Prework/Sing.java
@@ -3,14 +3,8 @@ public class Sing {
   public static void main(String[] args) {
   String[] Lyrics = { "She was trapped in a mirror", "And it couldn't be clearer", "She wanted to leave this place", "And get herself back in space", "And dad you might think she's a criminal", "But her friendship comes through subliminal", "Lapis Lazuli, you fled into the bottom of the sea", "Lapis Lazuli, you were so mad, but then you came around to me" };
 
-    System.out.println(Lyrics[0]);
-    System.out.println(Lyrics[1]);
-    System.out.println(Lyrics[2]);
-    System.out.println(Lyrics[3]);
-    System.out.println(Lyrics[4]);
-    System.out.println(Lyrics[5]);
-    System.out.println(Lyrics[6]);
-    System.out.println(Lyrics[7]);
+    for (String i: Lyrics) {
+      System.out.println(i);
+    }
   }
-
 }
