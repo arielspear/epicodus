26985e784e7b2526025fe3424757f543d41bbf88
Ariel Spear <simpleredfox@gmail.com>
2015-08-06 13:17:00 -0700

    Math

---
+++ Prework/math/Div.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+
+public class Div {
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+
+    System.out.println("Give me a number.");
+    String firstNumString = myConsole.readLine();
+    Integer firstNum = Integer.parseInt(firstNumString);
+
+    System.out.println("Give me another number.");
+    String secondNumString = myConsole.readLine();
+    Integer secondNum = Integer.parseInt(secondNumString);
+
+    Integer newNum = firstNum / secondNum;
+    System.out.println(firstNum + " / " + secondNum + " = " + newNum);
+  }
+}
